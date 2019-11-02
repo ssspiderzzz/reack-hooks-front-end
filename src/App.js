94983ce116721b9d2a9d78889b08ddd91c76656a
Haibin Zhu <ssspiderzzz@gmail.com>
2019-11-01 18:14:50 -0700

    change to new react hooks for the boilerplate, update dependencies version

--- src/App.js
+++ src/App.js
@@ -1,38 +1,28 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      message: 'Click the button to load data!'
-    }
-  }
+export default function App(props) {
+  const [state, setState] = useState("Click the button to load data!");
 
-  fetchData = () => {
-    axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
-    .then((response) => {
-      // handle success
-      console.log(response.data) // The entire response from the Rails API
+  function fetchData() {
+    axios
+      .get("/api/data") // You can simply make your requests to "/api/whatever you want"
+      .then(response => {
+        // handle success
+        console.log(response.data); // The entire response from the Rails API
 
-      console.log(response.data.message) // Just the message
-      this.setState({
-        message: response.data.message
+        console.log(response.data.message); // Just the message
+        setState({
+          message: response.data.message
+        });
       });
-    }) 
   }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>{ this.state.message }</h1>
-        <button onClick={this.fetchData} >
-          Fetch Data
-        </button>        
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1>{state.message}</h1>
+      <button onClick={fetchData}>Fetch Data</button>
+    </div>
+  );
 }
-
-export default App;
